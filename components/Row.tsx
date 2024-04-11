89195a805323c3ab7a2c464a3e0b8cd9e9a6d115
= <=>
2024-04-11 15:32:56 -0700

    Refactor animations

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,28 +1,19 @@
+import { emptyArray } from "@/constants/guess";
 import { FormattedGuess } from "@/types/wordleTypes";
-import clsx from "clsx";
 
 type Props = {
   guess: FormattedGuess[];
   currentGuess?: string;
 };
 
-const emptyRowArr = [1, 2, 3, 4, 5];
-
 const Row = ({ guess, currentGuess }: Props) => {
   if (guess) {
     return (
-      <div className="flex text-center justify-center">
+      <div className="row">
         {guess.map((letter, index) => (
           <div
             key={`${letter}-${index}`}
-            className={clsx(
-              "h-16 w-16 border border-gray-200 m-1 text-center leading-[4rem] uppercase font-bold text-3xl rounded select-none text-white",
-              {
-                "bg-wordGreen border-wordGreen": letter.color === "green",
-                "bg-wordYellow border-wordYellow": letter.color === "yellow",
-                "bg-wordGrey border-wordGrey": letter.color === "grey",
-              }
-            )}
+            className={`${letter.color} text-white`}
           >
             {letter.key}
           </div>
@@ -33,12 +24,9 @@ const Row = ({ guess, currentGuess }: Props) => {
 
   if (currentGuess) {
     return (
-      <div className="flex text-center justify-center">
-        {[...Array(5)].map((letter, index) => (
-          <div
-            key={`${letter}-${index}`}
-            className="h-16 w-16 border border-gray-200 m-1 text-center leading-[4rem] uppercase font-bold text-3xl rounded select-none"
-          >
+      <div className="row current">
+        {emptyArray.map((value, index) => (
+          <div key={index} className={currentGuess[index] && "filled"}>
             {currentGuess[index]}
           </div>
         ))}
@@ -47,12 +35,9 @@ const Row = ({ guess, currentGuess }: Props) => {
   }
 
   return (
-    <div className="flex text-center justify-center">
-      {emptyRowArr.map((value) => (
-        <div
-          key={value}
-          className="h-16 w-16 border border-gray-200 m-1 text-center leading-[4rem] uppercase font-bold text-3xl rounded select-none"
-        ></div>
+    <div className="row">
+      {emptyArray.map((value, index) => (
+        <div key={index}></div>
       ))}
     </div>
   );
